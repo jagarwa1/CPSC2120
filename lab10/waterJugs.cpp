8239d025a107963ab53ff2474339d65451e36404
jagarwa1 <jaiveer.agarwal@gmail.com>
2023-04-24 13:46:40 -0400

    finished wolfGoatCabbage

--- lab10/waterJugs.cpp
+++ lab10/waterJugs.cpp
@@ -1,8 +1,9 @@
 /*
- * Name:
- * Date Submitted:
- * Lab Section:
- * Assignment Name:
+ * Name:            Jai Agarwal
+ * Date Submitted:  4/24/2023
+ * Lab Section:     002
+ * Assignment Name: lab 10: puzzles using breadth first search
+ *                          (wolf goat cabbage and water jugs)
  */
 
 #include <iostream>
